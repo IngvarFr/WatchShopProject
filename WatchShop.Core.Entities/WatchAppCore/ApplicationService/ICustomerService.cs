f6bd3c7b3d37893f10718f1f12b8da543203671c
Sub-Z3r0 <andersen19@gmail.com>
2018-10-08 11:13:56 +0200

    smá breyting, dependencies
    og interface klassi

--- WatchShop.Core.Entities/WatchAppCore/ApplicationService/ICustomerService.cs
+++ WatchShop.Core.Entities/WatchAppCore/ApplicationService/ICustomerService.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WatchShop.Core.Entity;
 
 namespace WatchAppCore.ApplicationService
 {
-    interface ICustomerService
+    public interface ICustomerService
     {
+        Customers GetCustomerInstance();
+
+        List<Customers> GetCustomers();
+
+        Customers AddCustomer(Customers owner);
+
+        void DeleteCustomer(int id);
+
+        List<Customers> FindCustomersName();
+
+        Customers FindCustomersById(int id);
+
+        Customers FindCustomersByIdIncludePets(int id);
+
+        Customers UpdateCustomers(Customers ownerUpdate);
     }
 }
+
