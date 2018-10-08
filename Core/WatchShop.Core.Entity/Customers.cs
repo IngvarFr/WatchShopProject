9ddb188b5e58ac4c28415a242f1f37f6204190f4
Sub-Z3r0 <andersen19@gmail.com>
2018-10-08 10:54:46 +0200

    bjó til class

---
+++ Core/WatchShop.Core.Entity/Customers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WatchShop.Core.Entity
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+    }
+}
